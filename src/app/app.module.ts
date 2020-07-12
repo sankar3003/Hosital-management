7e99ed504f39f29eefa7dcd2068dba8f44cf8db5
sankar303 <sankar30031997@gmail.com>
2020-07-12 20:43:09 +0530

    user page created and doing crud with php

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
-import { DefaultComponent } from './layouts/default/default.component';
-import { AddHospitalComponent } from './pages/add-hospital/add-hospital.component';
-import { DefaultModule } from './layouts/default/default.module';
+import { ApplicationConfig } from './resources/application.config';
 
-import { SharedModule } from './shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,19 +7,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { httpErrorInterceptor } from './http-error.interceptor';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { HighlightDirective } from './highlight.directive';
 import { TestComponent } from './test/test.component';
+import { MydashboardComponent } from './mydashboard/mydashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [AppComponent, HighlightDirective, TestComponent],
-  imports: [BrowserAnimationsModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, HighlightDirective, TestComponent, MydashboardComponent],
+  imports: [BrowserAnimationsModule, AppRoutingModule, HttpClientModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, LayoutModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: httpErrorInterceptor,
       multi: true,
     },
+    ApplicationConfig,
   ],
   bootstrap: [AppComponent],
 })
